9a771cf1842bc846e64ed3ff4b116077eedeeaa1
kwqai <kwq2017@gmail.comgit config --global user.email kwq2017@gmail.com>
2024-03-30 00:32:50 -0700

    added console at line 6

--- main.ts
+++ main.ts
@@ -3,6 +3,8 @@
 
 import inquirer from "inquirer";
 
+console.log("Welcome To Easy Calculator");
+
 const answer = await inquirer.prompt([
     { message: "Enter First Number", type: "number", name: "firstNumber" },
     { message: "Enter Second Number", type: "number", name: "secondNumber" },
